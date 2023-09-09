52339a3408068a16f6a2f2ec1018843a88b2393c
Enijar <TheEnijar@gmail.com>
2023-09-09 16:09:11 +0100

    chore: clean up server code

--- src/server.tsx
+++ src/server.tsx
@@ -3,43 +3,43 @@ import { renderToReadableStream } from "react-dom/server";
 import Client from "./client";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 
-export default async function server() {
-  Bun.serve({
-    async fetch(req: Request) {
-      const url = new URL(req.url);
-      const currentFile = new URL(import.meta.url);
-      const buildPath = currentFile.pathname.replace(/\/src\/server\.tsx$/, "/build");
-      const filePath = buildPath + url.pathname;
-      const file = Bun.file(filePath);
-      if (!(await file.exists())) {
-        const sheet = new ServerStyleSheet();
-        try {
-          const jsx = sheet.collectStyles(
-            <StyleSheetManager sheet={sheet.instance}>
-              <Client />
-            </StyleSheetManager>,
-          );
-          await renderToReadableStream(jsx);
-          const styles = sheet.getStyleElement();
-          const stream = await renderToReadableStream(<Client styles={styles} />);
-          return new Response(stream, {
-            headers: {
-              "Content-Type": "text/html",
-            },
-          });
-        } catch (err) {
-          // todo: handle error
-          console.error(err);
-        } finally {
-          sheet.seal();
-        }
-      }
-      return new Response(file.stream(), {
+export async function fetch(req: Request) {
+  const url = new URL(req.url);
+  const currentFile = new URL(import.meta.url);
+  const buildPath = currentFile.pathname.replace(/\/src\/server\.tsx$/, "/build");
+  const filePath = buildPath + url.pathname;
+  const file = Bun.file(filePath);
+  if (!(await file.exists())) {
+    const sheet = new ServerStyleSheet();
+    try {
+      const jsx = sheet.collectStyles(
+        <StyleSheetManager sheet={sheet.instance}>
+          <Client />
+        </StyleSheetManager>,
+      );
+      await renderToReadableStream(jsx);
+      const styles = sheet.getStyleElement();
+      const stream = await renderToReadableStream(<Client styles={styles} />);
+      return new Response(stream, {
         headers: {
-          "Content-Type": file.type,
-          "Content-Length": file.size.toString(),
+          "Content-Type": "text/html",
         },
       });
+    } catch (err) {
+      // todo: handle error
+      console.error(err);
+    } finally {
+      sheet.seal();
+    }
+  }
+  return new Response(file.stream(), {
+    headers: {
+      "Content-Type": file.type,
+      "Content-Length": file.size.toString(),
     },
   });
+}
+
+export default function server() {
+  Bun.serve({ fetch });
 }
